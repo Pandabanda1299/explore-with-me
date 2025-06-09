2745033b19820cfbd756e7022ef7322b3208b999
Zubarev Dmitry <dmitryshikary@yandex.ru>
2025-06-09 20:02:51 +0400

    docker-compose bad up

--- stats/stats-client/src/main/java/stats/exception/ErrorResponse.java
+++
@@ -1,9 +0,0 @@
-package stats.exception;
-
-import lombok.Value;
-
-@Value
-public class ErrorResponse {
-    String error;
-    String description;
-}
--- stats/stats-client/src/main/java/stats/exception/InternalErrorException.java
+++
@@ -1,7 +0,0 @@
-package stats.exception;
-
-public class InternalErrorException extends RuntimeException {
-    public InternalErrorException(String message) {
-        super(message);
-    }
-}
